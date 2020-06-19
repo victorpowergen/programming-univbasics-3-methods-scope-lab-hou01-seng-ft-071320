67a2df94411a1f3f734a5a81572570856a7da078
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-19 05:12:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,26 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
+
+
+$status = "Thank You Mario! But Our Princess Is In Another Castle!"
+$phrase = "It's-a me, Mario!" 
+$phrase_link = "It's Dangerous To Go Alone! Take This." 
+
+def mario 
+  puts $status
 
 def toadstool
-  puts status
+puts $phrase
+  
+
+def link
+   puts $phrase_link
+  
+
+def all_phrases
+  puts $status
+  puts $phrase
+  puts $phrase_link
+ 
+  end
 end
+ 
+all_phrases
